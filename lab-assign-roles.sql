e882463982babad6d8a765731306b3c130f767b0
Matthew Pearson <matthewp@linuxacademy.com>
2019-04-29 13:42:42 -0400

    sql for assigning roles lab

---
+++ lab-assign-roles.sql
@@ -0,0 +1,16 @@
+CREATE USER 'corey'@'localhost' IDENTIFIED BY 'Linux4you!';
+CREATE USER 'will'@'localhost' IDENTIFIED BY 'Linux4you!';
+CREATE USER 'aaron'@'localhost' IDENTIFIED BY 'Linux4you!';
+CREATE USER 'kenny'@'localhost' IDENTIFIED BY 'Linux4you!';
+CREATE USER 'myles'@'localhost' IDENTIFIED BY 'Linux4you!';
+CREATE USER 'mike'@'localhost' IDENTIFIED BY 'Linux4you!';
+CREATE DATABASE dev;
+CREATE DATABASE prod;
+CREATE TABLE dev.products (productID INT PRIMARY KEY, productType VARCHAR(50), operatingSystem VARCHAR(50));
+CREATE TABLE prod.products (productID INT PRIMARY KEY, productType VARCHAR(50), operatingSystem VARCHAR(50));
+CREATE TABLE dev.orders (orderID INT PRIMARY KEY, userName VARCHAR(50) UNIQUE, orderType VARCHAR(255), purchaseDate DATE);
+CREATE TABLE prod.orders (orderID INT PRIMARY KEY, userName VARCHAR(50) UNIQUE, orderType VARCHAR(255), purchaseDate DATE);
+INSERT INTO prod.products (productID,productType,operatingSystem) VALUES (1,'laptop','ubuntu'),(2,'desktop','windows'),(3,'server','rhel');
+INSERT INTO prod.orders (orderID,userName,orderType,purchaseDate) VALUES (1,'aaron','laptop','2018-02-23'),(2,'stosh','desktop','2018-01-25'),(3,'kenny','server','2019-12-10');
+INSERT INTO dev.products (productID,productType,operatingSystem) VALUES (1,'laptop','ubuntu'),(2,'desktop','windows'),(3,'server','rhel');
+INSERT INTO dev.orders (orderID,userName,orderType,purchaseDate) VALUES (1,'aaron','laptop','2018-02-23'),(2,'stosh','desktop','2018-01-25'),(3,'kenny','server','2019-12-10');
